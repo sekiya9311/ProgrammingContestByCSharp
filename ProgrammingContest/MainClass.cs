f5e1d9aae928f1a1e5f44f23ad4ebd2659722e12
sekiya9311 <sekiya9311@gmail.com>
2019-07-06 22:40:49 +0900

    upd template class

--- ProgrammingContest/MainClass.cs
+++ ProgrammingContest/MainClass.cs
@@ -3,14 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Numerics;
 
 namespace ProgrammingContest
 {
     class Writer : IDisposable
     {
-        private System.IO.TextWriter Out { get; }
-        private StringBuilder Sb { get; }
-        private bool IsReactive { get; }
+        private readonly System.IO.TextWriter _out;
+        private readonly StringBuilder _outCache;
+        private readonly bool _isReactice;
+
         public Writer(string path)
             : this(new System.IO.StreamWriter(path))
         {
@@ -21,51 +23,51 @@ namespace ProgrammingContest
         }
         public Writer(System.IO.TextWriter writer = null, bool isReactive = false)
         {
-            this.Out = (writer ?? Console.Out);
-            this.IsReactive = isReactive;
-            if (!this.IsReactive)
+            this._out = (writer ?? Console.Out);
+            this._isReactice = isReactive;
+            if (!this._isReactice)
             {
-                this.Sb = new StringBuilder();
+                this._outCache = new StringBuilder();
             }
         }
         public void Dispose()
         {
-            if (!this.IsReactive)
+            if (!this._isReactice)
             {
-                this.Out.Write(Sb.ToString());
+                this._out.Write(_outCache.ToString());
             }
-            if (!this.Out.Equals(Console.Out))
+            if (!this._out.Equals(Console.Out))
             {
-                this.Out.Dispose();
+                this._out.Dispose();
             }
         }
         public void Write(object val)
         {
-            if (this.IsReactive)
+            if (this._isReactice)
             {
-                this.Out.Write(val.ToString());
-                this.Out.Flush();
+                this._out.Write(val.ToString());
+                this._out.Flush();
             }
             else
             {
-                this.Sb.Append(val.ToString());
+                this._outCache.Append(val.ToString());
             }
         }
         public void WriteFormat(string format, params object[] vals)
         {
-            if (this.IsReactive)
+            if (this._isReactice)
             {
-                this.Out.Write(format, vals);
-                this.Out.Flush();
+                this._out.Write(format, vals);
+                this._out.Flush();
             }
             else
             {
-                this.Sb.AppendFormat(format, vals);
+                this._outCache.AppendFormat(format, vals);
             }
         }
 
         public void WriteLine(object val = null)
-            => this.WriteFormat(val?.ToString() ?? string.Empty + Environment.NewLine);
+            => this.WriteFormat((val?.ToString() ?? string.Empty) + Environment.NewLine);
 
         public void WriteLine(int val)
             => this.WriteLine(val.ToString());
@@ -82,85 +84,96 @@ namespace ProgrammingContest
 
     class Scanner : IDisposable
     {
-        private Queue<string> Buffer { get; }
-        private char[] Sep { get; }
-        private System.IO.TextReader Reader { get; }
+        private readonly Queue<string> _buffer;
+        private readonly char[] _sep;
+        private readonly System.IO.TextReader _reader;
+
         public Scanner(string path, char[] sep = null)
             : this(new System.IO.StreamReader(path), sep)
         {
         }
-        public Scanner(System.IO.TextReader reader = null,
-                        char[] sep = null)
+        public Scanner(
+            System.IO.TextReader reader = null,
+            char[] sep = null)
         {
-            this.Buffer = new Queue<string>();
-            this.Sep = (sep ?? new char[] { ' ' });
-            this.Reader = (reader ?? Console.In);
+            this._buffer = new Queue<string>();
+            this._sep = (sep ?? new char[] { ' ' });
+            this._reader = (reader ?? Console.In);
         }
         private void CheckBuffer()
         {
-            if (this.Buffer.Count == 0 && this.Reader.Peek() != -1)
+            if (this._buffer.Count == 0 && this._reader.Peek() != -1)
             {
                 string str = string.Empty;
                 for (; string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-                str = this.Reader.ReadLine()) ;
+                str = this._reader.ReadLine()) ;
 
-                var strs = str.Split(this.Sep).Where(el => !(string.IsNullOrEmpty(el) || string.IsNullOrWhiteSpace(el)));
+                var strs = str.Split(this._sep)
+                    .Where(el => !(string.IsNullOrEmpty(el) || string.IsNullOrWhiteSpace(el)));
                 foreach (var el in strs)
                 {
-                    this.Buffer.Enqueue(el);
+                    this._buffer.Enqueue(el);
                 }
             }
         }
 
         public void Dispose()
         {
-            if (!this.Reader.Equals(Console.In))
+            if (!this._reader.Equals(Console.In))
             {
-                this.Reader.Dispose();
+                this._reader.Dispose();
             }
         }
 
         public string Next()
         {
             this.CheckBuffer();
-            return this.Buffer.Dequeue();
+            return this._buffer.Dequeue();
         }
 
         public string[] GetStringArray(int N)
             => Enumerable.Range(0, N)
-                         .Select(e => this.Next())
-                         .ToArray();
+                .Select(e => this.Next())
+                .ToArray();
 
         public int NextInt()
             => int.Parse(this.Next());
 
         public int[] GetIntArray(int N)
             => Enumerable.Range(0, N)
-                         .Select(e => this.NextInt())
-                         .ToArray();
+                .Select(e => this.NextInt())
+                .ToArray();
 
         public double NextDouble()
             => double.Parse(this.Next());
 
         public double[] GetdoubleArray(int N)
             => Enumerable.Range(0, N)
-                         .Select(e => this.NextDouble())
-                         .ToArray();
+                .Select(e => this.NextDouble())
+                .ToArray();
 
         public long NextLong()
             => long.Parse(this.Next());
 
         public long[] GetLongArray(int N)
             => Enumerable.Range(0, N)
-                         .Select(e => this.NextLong())
-                         .ToArray();
+                .Select(e => this.NextLong())
+                .ToArray();
+
+        public BigInteger NextBigInt()
+            => BigInteger.Parse(this.Next());
+
+        public BigInteger[] GetBigIntArray(int N)
+            => Enumerable.Range(0, N)
+                .Select(e => this.NextBigInt())
+                .ToArray();
 
         public bool IsEnd
         {
             get
             {
                 this.CheckBuffer();
-                return this.Buffer.Count == 0;
+                return this._buffer.Count == 0;
             }
         }
     }
@@ -169,8 +182,8 @@ namespace ProgrammingContest
     {
         private Scanner Sc { get; }
         private Writer Wr { get; }
-        private string InFilePath { get; } = "in.txt";
-        private string OutFilePath { get; } = "out.txt";
+        private string InFilePath => "in.txt";
+        private string OutFilePath => "out.txt";
         public MainClass()
         {
             this.Wr = new Writer(this.IsReactive);
@@ -206,9 +219,9 @@ namespace ProgrammingContest
 
         void Solve()
         {
-            Wr.WriteLine(Sc.Next());
+            
         }
 
-        private bool IsReactive { get; } = false; // TODO: reactive check !!
+        private bool IsReactive => false; // TODO: true, if reactive problem
     }
 }
